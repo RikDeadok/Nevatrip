8756c0018b5383dd3e3b5ab3195f7897f0e8c242
Sergey Kovalenko <rikdeadok@gmail.com>
2022-10-16 17:38:04 +0600

    initial new project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WelcomePage from './modules/welcomePage/welcomePage';
+import './assets/scss/main.scss';
+
+function App() {
+  return (
+    <div>
+      <WelcomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/modules/welcomePage/welcomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function WelcomePage() {
+  return (
+    <div className="wrapper">
+      <div className="content">
+        <div className="container">
+          Hello
+        </div>
+      </div>
+    </div>
+  );
+}
+export default WelcomePage;
