69a4abb26e8bebc97aad33669a40c9a47d08e528
Sergey Kovalenko <rikdeadok@gmail.com>
2022-10-17 13:13:58 +0600

    added dinamic and fix style

--- src/modules/welcomePage/components/card/card.jsx
+++ src/modules/welcomePage/components/card/card.jsx
@@ -1,86 +1,78 @@
 import React from 'react';
-import photo from '../../../../assets/img/image.png';
+import dataCard from './dataCard';
 import './card.scss';
 
 function Card() {
   return (
     <div className="card">
-      <div className="card__wrapper">
-        <div className="card__body">
-          <div className="rotate">
-            <div className="card__image">
-              <img src={photo} alt={photo} />
-              <span className="card__baner _baner-yellow">Новинка</span>
-            </div>
-            <div className="card__description">
-              <div className="card__title">АКЦИЯ - Обзорная экскурсия по рекам и каналам  с остановками Hop on Hop Off 2019</div>
-              <div className="card__timestamp timestamp">
-                <div className="timestamp__icon">
-                  <span className="_icon-time" />
-                </div>
-                <div className="timestamp__description">2 часа</div>
+      {dataCard.map((item) => (
+        <div className="card__wrapper">
+          <div className="card__body">
+            <div className="rotate">
+              <div className="card__image">
+                <img src={item.image} alt={item.image} />
+                <span className={item.status !== '' ? 'card__baner _baner-yellow' : 'card__baner'}>{item.status}</span>
               </div>
-              <div className="card__info info-card">
-                <div className="row">
-                  <div className="info-card__icon">
-                    <span className="_icon-done" />
+              <div className="card__description">
+                <div className="card__title">{item.title}</div>
+                <div className="card__timestamp timestamp">
+                  <div className="timestamp__icon">
+                    <span className="_icon-time" />
                   </div>
-                  <div className="info-card__description">Билет на целый день</div>
+                  <div className="timestamp__description">{item.timestamp}</div>
                 </div>
-                <div className="row">
-                  <div className="info-card__icon">
-                    <span className="_icon-done" />
+                <div className="card__info info-card">
+                  <div className="row">
+                    <div className="info-card__icon">
+                      <span className="_icon-done" />
+                    </div>
+                    <div className="info-card__description">Билет на целый день</div>
                   </div>
-                  <div className="info-card__description">Неограниченное число катаний</div>
-                </div>
-                <div className="row">
-                  <div className="info-card__icon">
-                    <span className="_icon-done" />
+                  <div className="row">
+                    <div className="info-card__icon">
+                      <span className="_icon-done" />
+                    </div>
+                    <div className="info-card__description">Неограниченное число катаний</div>
                   </div>
-                  <div className="info-card__description">6 остановок у главных достопримечательностей</div>
-                </div>
-                <div className="row">
-                  <div className="info-card__icon">
-                    <span className="_icon-done" />
+                  <div className="row">
+                    <div className="info-card__icon">
+                      <span className="_icon-done" />
+                    </div>
+                    <div className="info-card__description">6 остановок у главных достопримечательностей</div>
                   </div>
-                  <div className="info-card__description">
-                    Ближайший рейс сегодня
-                    <ul className="info-card__time time-list">
-                      <li className="time-list__item">
-                        <div className="time-list__link">
-                          12:00
-                        </div>
-                      </li>
-                      <li className="time-list__item">
-                        <div className="time-list__link">
-                          14:00
-                        </div>
-                      </li>
-                      <li className="time-list__item">
-                        <div className="time-list__link">
-                          еще...
-                        </div>
-                      </li>
-                    </ul>
+                  <div className="row">
+                    <div className="info-card__icon">
+                      <span className="_icon-done" />
+                    </div>
+                    <div className="info-card__description">
+                      Ближайший рейс сегодня
+                      <ul className="info-card__time time-list">
+                        {item.times.map((time) => (
+                          <li className="time-list__item">
+                            <div className="time-list__link">{time}</div>
+                          </li>
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="row last-row">
-                <div className="card__price price-card">
-                  <div className="price-card__price">
-                    900
-                    <span className="price-card__symbol">P</span>
+                <div className="row last-row">
+                  <div className="card__price price-card">
+                    <div className="price-card__price">
+                      {item.price}
+                      <span className="price-card__symbol">{item.currency}</span>
+                    </div>
+                    <div className="price-card__additional">{item.additional}</div>
                   </div>
-                  <div className="price-card__additional">1200 Р на причале</div>
-                </div>
-                <div className="card__button">
-                  <button className="button-filled" type="submit">Подробнее</button>
+                  <div className="card__button">
+                    <button className="button-filled" type="submit">Подробнее</button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
---
+++ src/modules/welcomePage/components/card/dataCard.js
@@ -0,0 +1,40 @@
+const dataCard = [
+  {
+    image: 'https://funart.pro/uploads/posts/2021-04/1618617077_3-funart_pro-p-sankt-peterburg-krasivie-mesta-foto-3.jpg',
+    status: 'Новинка',
+    title: 'АКЦИЯ - Обзорная экскурсия по рекам и каналам  с остановками Hop on Hop Off 2019',
+    timestamp: '2 часа',
+    times: [
+      '12:00', '14:00', '16:00', '18:00', '20:00',
+    ],
+    price: '900',
+    currency: '₽',
+    additional: '1200 Р на причале',
+  },
+  {
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUggqT_xBO1ZTWjUTUjbazo-wqFGgIoTQjaQ&usqp=CAU',
+    status: '',
+    title: 'АКЦИЯ - Обзорная экскурсия по рекам и каналам  с остановками Hop on Hop Off 2019',
+    timestamp: '2 часа',
+    times: [
+      '12:00', '14:00', '16:00', '18:00', '20:00',
+    ],
+    price: '900',
+    currency: '₽',
+    additional: '',
+  },
+  {
+    image: '',
+    status: '',
+    title: 'АКЦИЯ - Обзорная экскурсия по рекам и каналам  с остановками Hop on Hop Off 2019',
+    timestamp: '2 часа',
+    times: [
+      '12:00', '14:00', '16:00', '18:00', '20:00',
+    ],
+    price: '35',
+    currency: '$',
+    additional: '42 $ на причале',
+  },
+];
+
+export default dataCard;
